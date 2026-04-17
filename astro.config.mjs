5861f836f7ad65916a241fcf0c8e86bf82466d3b
aboodmidani <55650856+aboodmidani@users.noreply.github.com>
2026-04-17 16:49:12 +0200

    Major Changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,26 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
 
-// https://astro.build/config
 export default defineConfig({
-  site: 'https://aboodmidani.github.io',  // Replace with your actual domain
-  base: '/', // Important for GitHub Pages
-  integrations: [tailwind(), react()],
+  site: "https://aboodmidani.github.io",
+  base: "/",
+  integrations: [
+    tailwind(),
+    react({
+      jsxImportSource: "react",
+    }),
+  ],
   vite: {
     resolve: {
       alias: {
         "@": "/src",
         "@components": "/src/components",
       },
+    },
+    optimizeDeps: {
+      include: ["react", "react-dom"],
     },
   },
   output: "static",
